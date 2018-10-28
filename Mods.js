24faf3690aabf1e33240e1b29bd50aa5b03b1de0
ThetaHacker <42395099+ThetaHacker@users.noreply.github.com>
2018-10-28 20:09:26 +1100

    Create Mods.js

---
+++ Mods.js
@@ -0,0 +1,17 @@
+/* Fly Mod
+to deploy flying after install press "W",
+Then press Space
+Control up-down with "W" and "S"
+*\
+
+Runner.instance_.updateConfigSetting("INITIAL_JUMP_VELOCITY",1)
+var uptrig = document.body;
+    uptrig.addEventListener("keydown", function(e) {if (e.keyCode === 87) {
+        Runner.instance_.updateConfigSetting("GRAVITY",-2)
+		console.log("up")
+    }});
+var downtrig = document.body;
+    downtrig.addEventListener("keydown", function(e) {if (e.keyCode === 83) {
+        Runner.instance_.updateConfigSetting("GRAVITY",0.2)
+		console.log("nup")
+    }});
